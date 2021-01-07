8cd984f1dc80ec5af1e0551f6435a412e16bd0bc
Tony Hung <tbass134@gmail.com>
2021-01-07 08:08:16 -0500

    15-30

--- generators.py
+++ generators.py
@@ -129,9 +129,9 @@ def create_rst(destination_filename, with_ints=False, with_answers=False):
 
 
 if __name__ == '__main__':
-#     create_jupyter_notebook()
-#     create_jupyter_notebook_random_question()
-#     create_markdown()
-#     create_markdown(with_hints=False, with_solutions=True)
-#     create_markdown(with_hints=True, with_solutions=False)
-#     create_markdown(with_hints=True, with_solutions=True)
+    create_jupyter_notebook()
+    create_jupyter_notebook_random_question()
+    create_markdown()
+    create_markdown(with_hints=False, with_solutions=True)
+    create_markdown(with_hints=True, with_solutions=False)
+    create_markdown(with_hints=True, with_solutions=True)
